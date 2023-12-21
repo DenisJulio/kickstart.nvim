8b0a1d45670203b4619fd55949696dc9184e6b2d
Denis Julio <djms1234@gmail.com>
2023-12-20 21:37:54 -0300

    Adds further configuration for jdtlsc
    * moves plugins to its own file
    * adds autopairs plugin
    * adds .editorconfig file

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,12 +1,100 @@
+-- Modules
 local util = require("util")
+local jdtls = require("jdtls")
 
+-- Variables
 local mason_path = vim.fn.expand("~/.local/share/nvim/mason")
 local jdtls_install = mason_path .. "/bin/jdtls"
--- print(jdtls_install)
+local root_markers = { 'gradlew', '.git', 'mvnw' }
+local root_dir = require("jdtls.setup").find_root(root_markers)
+local home = os.getenv("HOME")
+local project_name = vim.fn.fnamemodify(root_dir, ":p:h:t")
+local workspace_folder = home .. "/.cache/jdtls/workspace" .. project_name
+local path_to_mason_packages = home .. "/.local/share/nvim/mason/packages"
+local path_to_jdtls = path_to_mason_packages .. "/jdtls"
+local lombok_path = path_to_jdtls .. "/lombok.jar"
+local path_to_jar = path_to_jdtls .. "/plugins/org.eclipse.equinox.launcher_1.6.600.v20231106-1826.jar"
+
+
+-- Configuration
 local config = {
-    cmd = { jdtls_install },
-    capabilities = util.capabilities,
-    on_attach = util.on_attach,
-    root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
+  cmd = {
+    "java",
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-Xmx1g',
+    "-javaagent:" .. lombok_path,
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens',
+    'java.base/java.util=ALL-UNNAMED',
+    '--add-opens',
+    'java.base/java.lang=ALL-UNNAMED',
+    '-jar',
+    path_to_jar,
+    '-configuration', path_to_jdtls .. "/config_linux",
+    '-data', workspace_folder,
+  },
+  settings = {
+    java = {
+      format = {
+        enabled = true,
+        settings = {
+          url = vim.fn.stdpath("config") .. "/lang_servers/intellij-java-google-style.xml",
+          profile = "GoogleStyle",
+        }
+      },
+      eclipse = {
+        downloadSources = true,
+      },
+      maven = {
+        downloadSources = true,
+      },
+      signatureHelp = {
+        enable = true
+      },
+      contentProvider = {
+        preferred = "fernflower"
+      },
+      completion = {
+        favoriteStaticMembers = {
+          "org.hamcrest.MatcherAssert.assertThat",
+          "org.hamcrest.Matchers.*",
+          "org.hamcrest.CoreMatchers.*",
+          "org.junit.jupiter.api.Assertions.*",
+          "java.util.Objects.requireNonNull",
+          "java.util.Objects.requireNonNullElse",
+          "org.mockito.Mockito.*"
+        },
+        filteredTypes = {
+          "com.sun.*",
+          "io.micrometer.shaded.*",
+          "java.awt.*",
+          "jdk.*",
+          "sun.*",
+        },
+      },
+      sources = {
+        organizeImports = {
+          starThreshold = 9999,
+          staticStarThreshold = 9999,
+        },
+      },
+      codeGeneration = {
+        toString = {
+          template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}"
+        },
+        hashCodeEquals = {
+          useJava7Objects = true,
+        },
+        useBlocks = true,
+      },
+    }
+  },
+  capabilities = util.capabilities,
+  on_attach = util.on_attach,
+  root_dir = vim.fs.dirname(vim.fs.find(root_markers, { upward = true })[1]),
 }
-require('jdtls').start_or_attach(config)
+jdtls.start_or_attach(config)
